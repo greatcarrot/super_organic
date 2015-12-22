e7005ae6b6e074464638cd32efe2d4123e1b214b
Sava Jovanovic <hl1015@users.noreply.github.com>
2015-12-22 04:41:38 +0100

    changed number of columns in investment section, button fixes

--- super_organic/site/snippets/investments_section.php
+++ super_organic/site/snippets/investments_section.php
@@ -10,12 +10,12 @@
                   <?php $investments = $data->children()->visible()->flip() ?>
 
                   <?php foreach($investments as $article): ?>
-                  <div class="bbx-col-3">
+                  <div class="bbx-col-2">
                     <div class="bbx-frame-icon-box bbx-corner-details bbx-align-center bbx-4x-margin-bottom bbx-animation-on-appear fade_up">
                       <h5><a href="#" class="bbx-dark-link"><?php echo $article->title()->html() ?></a></h5>
                       <span class="bbx-divider-small"></span>
                       <p class="transparent-text-black"><?php echo $article->rightSectionText()->excerpt(60) ?></p>
-                      <a href="<?php echo $article->url() ?>" class="bbx-fancy-link bbx-block" style="padding-top:7px;">
+                      <a href="<?php echo $article->url() ?>" class="bbx-fancy-link bbx-block" style="padding-top:7px; max-width:200px;text-align: center; margin:auto;">
                         <span class="fancy-text">
                           Learn More
                                         </span>
